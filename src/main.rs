58783a498bb76943464f98150120f2de84293b58
Dusan Malusev <dusan@dusanmalusev.dev>
2024-05-21 20:33:37 +0200

    Implement multiple commands on same connection
    Signed-off-by: Dusan Malusev <dusan@dusanmalusev.dev>

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 
-mod parser;
 mod command;
+mod parser;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind("0.0.0.0:6379").await?;
 
     loop {
-        let (mut client, _socket) = listener.accept().await?;
+        let (client, _socket) = listener.accept().await?;
 
-        let mut buf = vec![0u8; 1024];
+        tokio::spawn(async move {
+            match handle_client(client).await {
+                Ok(_) => {}
+                Err(e) => {
+                    eprintln!("Error: {:?}", e);
+                }
+            }
+        });
+    }
+}
+
+async fn handle_client(mut client: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
+    let mut buf = vec![0u8; 65536];
+
+    loop {
         let nread = client.read(&mut buf).await?;
 
         let input = std::str::from_utf8(&buf[..nread]).unwrap();
